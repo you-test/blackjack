4e1029d7e530e5a2add9b43a0db8f4a25def9101
yusuke <sato11.jp@gmail.com>
2022-04-01 19:00:08 +0900

    fixed to multiplay

--- src/lib/Game.php
+++ src/lib/Game.php
@@ -52,37 +52,36 @@ class Game
             echo "プレイヤー{$no}の引いたカードは{$playerCard[1]->getCardString()}です。" . PHP_EOL;
         }
 
-        // ディーラーとカードと自分の現在の得点の表示
-        $firstResultMessage = <<<FIRST
-        ディーラーの引いたカードは{$dealerStringCards[0]}です。
-        ディーラーの引いた2枚目のカードはわかりません。
-        あなたの現在の得点は{$playerSum}です。カードを引きますか？（Y/N）
-        FIRST;
-        echo $firstResultMessage;
+        // ディーラーとのカードと自分の現在の得点の表示
+        echo "ディーラーの引いたカードは{$this->dealerCards[0]->getCardString()}です。" . PHP_EOL;
+        echo "ディーラーの引いた2枚目のカードはわかりません。" . PHP_EOL;
+        foreach ($this->players as $playerNo => $player) {
+            $playerSum = array_sum($player->getCardRanks());
+            $no = $playerNo + 1;
+            echo "プレイヤー{$no}の現在の得点は{$playerSum}です。カードを引きますか？（Y/N）" . PHP_EOL;
+        }
 
         // プレイヤーがカード引くか選択（ループ）
         $response = trim(fgets(STDIN));
         echo $response . PHP_EOL;
 
-        $playerScore = array_sum($player->getCardRanks());
         while ($response === 'Y' || $response === 'y') {
-            $drawedCard = $player->drawCard();
-            $this->playerCards[] = $drawedCard;
-            $playerScore = array_sum($player->getCardRanks());
-
-            echo "あなたの引いたカードは{$drawedCard->getCardString()}です。" . PHP_EOL;
-            echo "あなたの現在の得点は{$playerScore}です。カードを引きますか？（Y/N）" . PHP_EOL;
+            foreach ($this->players as $key => $player) {
+                $drawedCard = $player->drawCard();
+                $this->playerCards[$key][] = $drawedCard;
+                $playerScore = array_sum($player->getCardRanks());
+                echo "プレイヤー" . $key + 1 . "の引いたカードは{$drawedCard->getCardString()}です。" . PHP_EOL;
+                echo "プレイヤー" . $key + 1 . "の現在の得点は{$playerScore}です。" . PHP_EOL;
+            }
+            echo "カードを引きますか？（Y/N）";
             $response = trim(fgets(STDIN));
             echo $response;
         }
 
         // 最初にディーラーが引いた2枚目のカードとその合計値を表示
         $dealerFirstScore = array_sum($dealer->getCardRanks());
-        $dealerCardInfoMessage = <<< INFO
-        ディーラーの引いた2枚目のカードは{$dealerStringCards[1]}でした。
-        ディーラーの現在の得点は{$dealerFirstScore}です。
-        INFO;
-        echo $dealerCardInfoMessage . PHP_EOL;
+        echo "ディーラーの引いた2枚目のカードは{$this->dealerCards[1]->getCardString()}でした。" . PHP_EOL;
+        echo "ディーラーの現在の得点は{$dealerFirstScore}です。" . PHP_EOL;
 
         // ディーラーが合計値17以上になるまで引き続ける
         while (array_sum($dealer->getCardRanks()) < 17) {
@@ -94,11 +93,11 @@ class Game
 
         // 最終的な得点の表示
         $dealerScore = array_sum($dealer->getCardRanks());
-        $resultMessage = <<< RESULT
-        あなたの得点は{$playerScore}です。
-        ディーラーの得点は{$dealerScore}です。
-        RESULT;
-        echo $resultMessage . PHP_EOL;
+        foreach ($this->players as $key => $player) {
+            $playerScore = array_sum($player->getCardRanks());
+            echo "プレイヤー" . $key + 1 . "の得点は{$playerScore}です。" . PHP_EOL;
+        }
+        echo "ディーラーの得点は{$dealerScore}です。" . PHP_EOL;
 
         // 勝敗の表示
         $winnerEvaluator = new WinnerEvaluator();
